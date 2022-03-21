388aeeb7b493fac5a955ba93baad7b506829f09f
Christian Russo <christianrusso@MacBook-Pro-de-Christian.local>
2022-03-21 09:09:34 -0300

    cambio forma de usar el routing para el sidevar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,6 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
 
   ingresar() {
-    this.router.navigateByUrl('/dashboard');
+    this.router.navigateByUrl('/home');
   }
 }
