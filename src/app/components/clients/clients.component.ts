1ec4a39134988314c021328a0b1882bccd279ee9
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-04-01 10:01:06 -0300

    Pantalla ProductClient creada

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -70,7 +70,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 
 export class ClientsComponent implements AfterViewInit{
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit', 'editar', 'eliminar', 'ventas'];
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit', 'editar', 'eliminar', 'ventas', 'productos'];
   dataSource: MatTableDataSource<UserData>;
 
   matcher = new MyErrorStateMatcher();
