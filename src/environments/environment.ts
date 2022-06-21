76f368a4657017f0b75cb911a2e33cbf65274206
lucasrusman <rusmanlucas@gmail.com>
2022-06-21 09:44:30 -0300

    enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,14 @@
 
 export const environment = {
   production: false,
-  apiUsers: 'https://dyg-backend.herokuapp.com/api/user',
-  apiClients: 'https://dyg-backend.herokuapp.com/api/client',
-  apiProducts: 'https://dyg-backend.herokuapp.com/api/product',
-  apiProductsByCliente: 'https://dyg-backend.herokuapp.com/api/product/byClient',
-  apiSales: 'https://dyg-backend.herokuapp.com/api/sale',
-  apiLists: 'https://dyg-backend.herokuapp.com/api/list'
-
+  apiUsers: 'https://dyg-backend.herokuapp.com//api/user',
+  apiClients: 'https://dyg-backend.herokuapp.com//api/client',
+  apiProducts: 'https://dyg-backend.herokuapp.com//api/product',
+  apiProductsByCliente: 'https://dyg-backend.herokuapp.com//api/product/byClient',
+  apiSales: 'https://dyg-backend.herokuapp.com//api/sale',
+  apiLists: 'https://dyg-backend.herokuapp.com//api/list'
 };
-``;
+;
 
 /*
  * For easier debugging in development mode, you can import the following file
