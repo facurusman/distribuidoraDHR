4f16de7694b46010cd3e4da3bdc2996df36ce9c7
lucasrusman <rusmanlucas@gmail.com>
2022-06-21 09:47:51 -0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUsers: 'https://dyg-backend.herokuapp.com//api/user',
-  apiClients: 'https://dyg-backend.herokuapp.com//api/client',
-  apiProducts: 'https://dyg-backend.herokuapp.com//api/product',
-  apiProductsByCliente: 'https://dyg-backend.herokuapp.com//api/product/byClient',
-  apiSales: 'https://dyg-backend.herokuapp.com//api/sale',
-  apiLists: 'https://dyg-backend.herokuapp.com//api/list'
+  apiUsers: 'https://dyg-backend.herokuapp.com/api/user',
+  apiClients: 'https://dyg-backend.herokuapp.com/api/client',
+  apiProducts: 'https://dyg-backend.herokuapp.com/api/product',
+  apiProductsByCliente: 'https://dyg-backend.herokuapp.com/api/product/byClient',
+  apiSales: 'https://dyg-backend.herokuapp.com/api/sale',
+  apiLists: 'https://dyg-backend.herokuapp.com/api/list'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  apiUsers: 'https://dyg-backend.herokuapp.com//api/user',
-  apiClients: 'https://dyg-backend.herokuapp.com//api/client',
-  apiProducts: 'https://dyg-backend.herokuapp.com//api/product',
-  apiProductsByCliente: 'https://dyg-backend.herokuapp.com//api/product/byClient',
-  apiSales: 'https://dyg-backend.herokuapp.com//api/sale',
-  apiLists: 'https://dyg-backend.herokuapp.com//api/list'
+  apiUsers: 'https://dyg-backend.herokuapp.com/api/user',
+  apiClients: 'https://dyg-backend.herokuapp.com/api/client',
+  apiProducts: 'https://dyg-backend.herokuapp.com/api/product',
+  apiProductsByCliente: 'https://dyg-backend.herokuapp.com/api/product/byClient',
+  apiSales: 'https://dyg-backend.herokuapp.com/api/sale',
+  apiLists: 'https://dyg-backend.herokuapp.com/api/list'
 };
 ;
 
