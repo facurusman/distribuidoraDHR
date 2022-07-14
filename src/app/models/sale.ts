988f31b2e4064e7581540f1f193ff461071d0d54
Facundo <rusmanfacundo@gmail.com>
2022-07-14 12:31:45 -0300

    deuda

--- src/app/models/sale.ts
+++ src/app/models/sale.ts
@@ -2,18 +2,22 @@ export class Sale {
   readonly idCliente: number;
   readonly fecha: Date;
   readonly total: number;
+  readonly deuda: number;
 
   constructor({
     idCliente,
     fecha,
-    total
+    total,
+    deuda
   }: {
     idCliente: number;
     fecha: Date;
     total: number;
+    deuda: number;
   }) {
     this.idCliente = idCliente;
     this.fecha = fecha;
     this.total = total;
+    this.deuda = deuda;
   }
 }
