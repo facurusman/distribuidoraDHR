773ad6e97c56ef3fbbee56a4618ea153e4e3ee46
Facundo <rusmanfacundo@gmail.com>
2022-08-25 16:28:48 -0300

    avances en la exportacion del pdf

--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -1,5 +1,11 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { UntypedFormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {
+  UntypedFormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -17,7 +23,10 @@ import { PDFService } from 'src/app/services/pdf.service';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { SaleProductData } from 'src/app/models/SaleProductData';
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: UntypedFormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: UntypedFormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
@@ -28,18 +37,18 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
-  styleUrls: ['./sales.component.scss'],
+  styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent implements OnInit {
   emailFormControl = new UntypedFormControl('', [Validators.required, Validators.email]);
   selection = new SelectionModel<SaleData>(true, []);
   dataSourceVentas = new MatTableDataSource<SaleData>();
-  displayedColumnsVentas: string[] = ['select','id', 'idCliente', 'fecha', 'total', 'exports'];
-  @ViewChild('TableVentasSort', {static: true}) tableVentasSort: MatSort;
-  @ViewChild('TableVentasPaginator', {static: true}) tableVentasPaginator: MatPaginator;
-  fecha_inicial: any
-  fecha_final: any
-  id: number = 0
+  displayedColumnsVentas: string[] = ['select', 'id', 'idCliente', 'fecha', 'total', 'exports'];
+  @ViewChild('TableVentasSort', { static: true }) tableVentasSort: MatSort;
+  @ViewChild('TableVentasPaginator', { static: true }) tableVentasPaginator: MatPaginator;
+  fecha_inicial: any;
+  fecha_final: any;
+  id: number = 0;
   ventasSeleccionadas: SaleProductData[] = [];
   idCliente: number;
   constructor(
@@ -48,11 +57,12 @@ export class SalesComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private pdfService: PDFService,
-    private readonly clientService: ClientsService) {
+    private readonly clientService: ClientsService
+  ) {
     this.id = this.route.snapshot.params['id'];
     // Assign the data to the data source for the table to render
     this.dataSourceVentas = new MatTableDataSource();
-    this.getSales()
+    this.getSales();
   }
   matcher = new MyErrorStateMatcher();
   ngOnInit() {
@@ -61,44 +71,44 @@ export class SalesComponent implements OnInit {
   }
   getSales() {
     if (this.id) {
-      this.saleService.getSalesByClient(this.id).subscribe((response) => {
-        const sale = response as SaleData[]
-        this.dataSourceVentas.data = sale
-        sale.forEach(elemente =>{
-          elemente.fecha = elemente.fecha.slice(0,-14)
+      this.saleService.getSalesByClient(this.id).subscribe(response => {
+        const sale = response as SaleData[];
+        this.dataSourceVentas.data = sale;
+        sale.forEach(elemente => {
+          elemente.fecha = elemente.fecha.slice(0, -14);
         });
-      })
+      });
     } else {
-      this.saleService.getSales().subscribe((response) => {
-        const sale = response as SaleData[]
-        sale.forEach(elemente =>{
-          elemente.fecha = elemente.fecha.slice(0,-14)
+      this.saleService.getSales().subscribe(response => {
+        const sale = response as SaleData[];
+        sale.forEach(elemente => {
+          elemente.fecha = elemente.fecha.slice(0, -14);
         });
 
-        this.dataSourceVentas.data = sale
+        this.dataSourceVentas.data = sale;
       });
     }
   }
-  allSalesClient(id : number) {
-      this.saleService.getSalesByClient(id).subscribe((response) => {
-        const sales = response as ProductData[]
-    })
+  allSalesClient(id: number) {
+    this.saleService.getSalesByClient(id).subscribe(response => {
+      const sales = response as ProductData[];
+    });
   }
   updateTableFilter() {
-    this.saleService.filterSale(this.fecha_inicial, this.fecha_final).subscribe((response) => {
-      const sales = response as SaleProductData[]
+    this.saleService.filterSale(this.fecha_inicial, this.fecha_final).subscribe(response => {
+      const sales = response as SaleProductData[];
       let idSeleccionados: any = [];
       this.ventasSeleccionadas.forEach(vs => {
-        idSeleccionados.push(vs.id)
-      })
+        idSeleccionados.push(vs.id);
+      });
       sales.forEach(s => {
-        s.fecha = s.fecha.slice(0,-14)
-        if(idSeleccionados.includes(s.id)){
-          s.selected = true
-          s = s.fecha.slice(0,-14)
+        s.fecha = s.fecha.slice(0, -14);
+        if (idSeleccionados.includes(s.id)) {
+          s.selected = true;
+          s = s.fecha.slice(0, -14);
         }
-      })
-      this.dataSourceVentas.data = sales
+      });
+      this.dataSourceVentas.data = sales;
     });
   }
   isAllSelected() {
@@ -107,13 +117,13 @@ export class SalesComponent implements OnInit {
     return numSelected === numRows;
   }
   masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSourceVentas.data.forEach(row => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSourceVentas.data.forEach(row => this.selection.select(row));
   }
   clickEnSelector(idCliente: number) {
-    this.allSalesClient(idCliente)
-    this.idCliente = idCliente
+    this.allSalesClient(idCliente);
+    this.idCliente = idCliente;
   }
   applyFilterVentas(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -122,17 +132,17 @@ export class SalesComponent implements OnInit {
       this.dataSourceVentas.paginator.firstPage();
     }
   }
-  marcar(ob: MatCheckboxChange, row:SaleProductData) {
+  marcar(ob: MatCheckboxChange, row: SaleProductData) {
     if (ob.checked) {
       this.ventasSeleccionadas.push(row);
       row.selected = true;
     } else {
-      this.ventasSeleccionadas = this.ventasSeleccionadas.filter(p => p.id != row.id)
+      this.ventasSeleccionadas = this.ventasSeleccionadas.filter(p => p.id != row.id);
       row.selected = false;
     }
   }
 
-  longitudVentasChequeadas(){
+  longitudVentasChequeadas() {
     if (this.ventasSeleccionadas.length > 0) {
       return true;
     }
@@ -140,9 +150,9 @@ export class SalesComponent implements OnInit {
   }
 
   generarPDF() {
-      this.pdfService.generarPDFVentas().subscribe((response: any) => {
+    this.pdfService.generarPDFVentas().subscribe((response: any) => {
       const source = `data:application/pdf;base64,${response.finalString}`;
-      const link = document.createElement("a");
+      const link = document.createElement('a');
       link.href = source;
       link.download = `ventas.pdf`;
       link.click();
@@ -150,34 +160,46 @@ export class SalesComponent implements OnInit {
   }
 
   generarPDFClients() {
-      this.pdfService.generarPDFVentaClient(this.ventasSeleccionadas).subscribe((response: any) => {
+    this.pdfService.generarPDFVentaClient(this.ventasSeleccionadas).subscribe((response: any) => {
       const source = `data:application/pdf;base64,${response.finalString}`;
-      const link = document.createElement("a");
+      const link = document.createElement('a');
       link.href = source;
       link.download = `ventaCliente.pdf`;
       link.click();
     });
   }
   generarPDFProducts() {
-      this.pdfService.generarPDFVentaProduct(this.ventasSeleccionadas).subscribe((response: any) => {
+    this.pdfService.generarPDFVentaProduct(this.ventasSeleccionadas).subscribe((response: any) => {
       const source = `data:application/pdf;base64,${response.finalString}`;
-      const link = document.createElement("a");
+      const link = document.createElement('a');
       link.href = source;
       link.download = `ventaProducto.pdf`;
       link.click();
     });
   }
 
+  exportarPDFUnaSolaVenta(idCliente: number, idVenta: number) {
+    try {
+      this.saleService.getPropertiesClient(idCliente, idVenta).subscribe(async (response: any) => {
+        if ((await response.Status) == 200) {
+          const source = `data:application/pdf;base64,${await response.finalString}`;
+          const link = document.createElement('a');
+          link.href = source;
+          link.download = `ventaProducto.pdf`;
+          link.click();
+        } else {
+          setTimeout(() => {
+            alert('Error al intentar exportar la venta');
+            location.reload();
+          }, 1000);
+          throw new Error('Error al intentar exportar la venta');
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error('Error al intentar ejecutar la query');
+    }
 
-  exportarPDFUnaSolaVenta(idCliente : number, idVenta: number){
-
-    this.saleService.getPropertiesClient(idCliente, idVenta).subscribe( (response:any) => {
-      const source = `data:application/pdf;base64,${response.finalString}`;
-      const link = document.createElement("a");
-      link.href = source;
-      link.download = `ventaProducto.pdf`;
-      link.click();
-    })
   }
 
   gotoCreateSale() {
